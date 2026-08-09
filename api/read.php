bacc1b30d866571e4f2d8e774c0a93b03ca83987
camby <omorimbaram@gmail.com>
2026-08-09 15:53:33 +0000

    read file

--- api/read.php
+++ api/read.php
@@ -6,6 +6,9 @@ header('Content-Typr: Application/json');
 // initializion our api which everything we have // what we are doing is just to call the included  file to this section.
 include_once('../core/initialize.php');
 
-//instantiate post
+//instantiate post. // the db is passed here to the post class as we are inheriting it from the config.php file
 
 $post =new Post($db);
+
+//blog post query  this is what we are doing is just to call the included  file to this section.
+$result = $post->read();
